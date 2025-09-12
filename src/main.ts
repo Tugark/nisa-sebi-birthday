f38b9277e0aa6517b6056dd1310c43598eb38745
Tugark <lukemerz@gmail.com>
2025-09-12 15:32:06 +0200

    Add deployment

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,34 @@ import {Map, Marker, Popup} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import type {Point} from "geojson";
 
+type Rating = {
+    text: string;
+    author: string;
+}
 type Location = {
     title: string;
     description: string;
-    ratingNegative: string;
-    ratingPositive: string;
+    ratingNegative: Rating;
+    ratingPositive: Rating;
     location: Point;
+    image: string;
+    details: string;
 };
 
 const map = new Map({
     container: 'map', // container id
     style: 'https://tiles.openfreemap.org/styles/liberty',
     center: [172.463, -41.2],
-    zoom: 6
+    zoom: 6,
+    maxBounds: [
+        [156.0, -57.5], // southwest coordinates (lng, lat)
+        [189.0, -14.0]  // northeast coordinates (lng, lat)
+    ]
 });
 
 
 map.on('load', async () => {
-    await fetch('/data.json').then(async res => {
+    await fetch('./data.json').then(async res => {
         const data: Location[] = await res.json();
         data.forEach((item) => {
             new Marker()
@@ -32,10 +42,28 @@ map.on('load', async () => {
 });
 
 function generatePopupForLocation(item: Location): Popup {
-    return new Popup({offset: 25}).setHTML(`
-        <h3>${item.title}</h3>
-        <p>${item.description}</p>
-        <p>Positive reviews: ${item.ratingPositive}</p>
-        <p>Negative reviews: ${item.ratingNegative}</p>
+    return new Popup({offset: 25, focusAfterOpen: false, closeButton: false, className: "popup"}).setHTML(`
+        <img src="${item.image}&t=${Math.random()}" alt="${item.title}" class="img-fluid rounded mx-auto d-block" style="max-height: 300px;"/>
+        <h3 class="display-6">${item.title}</h3>
+        <p class="lead">${item.description}</p>
+        <hr>
+        <h4>What Google Reviewers say</h4>
+        <figure>
+          <blockquote class="blockquote">
+            <p>${item.ratingPositive.text}</p>
+          </blockquote>
+          <figcaption class="blockquote-footer">
+            ★★★★★ by ${item.ratingPositive.author}
+          </figcaption>
+        </figure>
+        <figure>
+          <blockquote class="blockquote">
+            <p>${item.ratingNegative.text}</p>
+          </blockquote>
+          <figcaption class="blockquote-footer">
+            ★☆☆☆☆ by ${item.ratingNegative.author}
+          </figcaption>
+        </figure>
+        <a class="btn btn-primary d-block" href="${item.details}" role="button" target="_blank" rel="noopener nofollow noreferrer">Details</a>
     `)
 }
