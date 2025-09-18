984f5913fd9defc5da725fc2fb887c77aa95c09b
Tugark <Tugark@users.noreply.github.com>
2025-09-18 14:17:08 +0000

    deploy: 3327ec6cb9b70e0fcc7d7da820a48cf8b71c6ebf

--- assets/surprise-kSJtpPUh.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap-icons-BzwDvIh3.js";(()=>{let t=4;const n=document.getElementById("countdown"),e=setInterval(()=>{t-=.1,t=Math.max(t,0),n.textContent=t.toFixed(1),t<=0&&clearInterval(e)},100)})();
